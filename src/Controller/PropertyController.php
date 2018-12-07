7ba8d8f53a00db9619946d43264e306a7bdca700
Meg4R0M <psykoterro@gmail.com>
2018-12-07 02:40:05 +0100

    Mise en place de la pagination avec KnpPaginator

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,12 +32,20 @@ class PropertyController extends AbstractController
 
     /**
      * @Route("/biens", name="property.index")
+     * @param PaginatorInterface $paginator
+     * @param Request $request
      * @return Response
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisibleQuery(),
+            $request->query->getInt('page', 1)/*page number*/,
+            12/*limit per page*/
+            );
         return $this->render('property/index.html.twig', [
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'properties' => $properties
         ]);
     }
 
--- src/DataFixtures/PropertyFixtures.php
+++ src/DataFixtures/PropertyFixtures.php
@@ -5,70 +5,32 @@ namespace App\DataFixtures;
 use App\Entity\Property;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PropertyFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $property = new Property();
-        $property->setAddress('1 Rue de la motte')
-            ->setBedrooms(4)
-            ->setCity('Nîmes')
-            ->setDescription('Description appart rue de la motte à Nîmes')
-            ->setFloor(2)
-            ->setHeat(1)
-            ->setPostalCode('30000')
-            ->setPrice(250000)
-            ->setRooms(6)
-            ->setSurface(120)
-            ->setTitle('Superbe appart Nîmes');
+        $faker = Factory::create('fr_FR');
 
-        $manager->persist($property);
+        for ($i = 0; $i < 100; $i++) {
+            $property = new Property();
 
-        $property2 = new Property();
-        $property2->setAddress('2 Avenue du boulodrome')
-            ->setBedrooms(2)
-            ->setCity('Marseille')
-            ->setDescription('Description appart Avenue du boulodrome à Marseille')
-            ->setFloor(1)
-            ->setHeat(0)
-            ->setPostalCode('13000')
-            ->setPrice(300000)
-            ->setRooms(2)
-            ->setSurface(80)
-            ->setTitle('Superbe appart Marseille');
+            $property->setAddress($faker->address)
+                ->setBedrooms($faker->numberBetween(1,9))
+                ->setCity($faker->city)
+                ->setDescription($faker->sentences(3, true))
+                ->setFloor($faker->numberBetween(0,15))
+                ->setHeat($faker->numberBetween(0, count(Property::HEAT) - 1))
+                ->setPostalCode($faker->postcode)
+                ->setPrice($faker->numberBetween(35000,1000000))
+                ->setRooms($faker->numberBetween(1,10))
+                ->setSurface($faker->numberBetween(20,350))
+                ->setTitle($faker->unique()->words(3, true));
 
-        $manager->persist($property2);
+            $manager->persist($property);
+        }
 
-        $property3 = new Property();
-        $property3->setAddress('3 Chemin du mas')
-            ->setBedrooms(4)
-            ->setCity('Montpellier')
-            ->setDescription('Description appart Chemin du mas à Montpellier')
-            ->setFloor(1)
-            ->setHeat(1)
-            ->setPostalCode('34000')
-            ->setPrice(320000)
-            ->setRooms(4)
-            ->setSurface(90)
-            ->setTitle('Superbe appart Montpellier');
-
-        $manager->persist($property3);
-
-        $property4 = new Property();
-        $property4->setAddress('4 lotissement du hameaux')
-            ->setBedrooms(2)
-            ->setCity('Narbonne')
-            ->setDescription('Description appart lotissement du hameaux à Narbonne')
-            ->setFloor(4)
-            ->setHeat(0)
-            ->setPostalCode('11000')
-            ->setPrice(180000)
-            ->setRooms(3)
-            ->setSurface(60)
-            ->setTitle('Superbe appart Narbonne');
-
-        $manager->persist($property4);
 
         $manager->flush();
     }
